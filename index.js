5386f7cede1b3b82f386508932b40208b8c43613
Andy Aldridge <andy@grange85.co.uk>
2018-08-12 23:06:30 +0100

    install googleapis

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const fs = require('fs');
 const twit = require('twit');
+const readline = require('readline');
+const {google} = require('googleapis');
 
-
-//var cred = JSON.parse(fs.readFileSync('credentials.json'));
-//console.log(cred.consumer_key);
+console.log(isHeroku());
+console.log(process.env.NODE);
+if (!isHeroku()){
+  var cred = JSON.parse(fs.readFileSync('credentials.json'));
+  console.log(cred.twitter.consumer_key);
+}
 
 var t = new twit({
-    consumer_key:         process.env.TWITTER_CONSUMER_KEY || cred.consumer_key,
-    consumer_secret:      process.env.TWITTER_CONSUMER_SECRET || cred.consumer_secret,
-    access_token:         process.env.TWITTER_ACCESS_TOKEN || cred.access_token,
-    access_token_secret:  process.env.TWITTER_ACCESS_TOKEN_SECRET || cred.access_token_secret
+    consumer_key:         process.env.TWITTER_CONSUMER_KEY || cred.twitter.consumer_key,
+    consumer_secret:      process.env.TWITTER_CONSUMER_SECRET || cred.twitter.consumer_secret,
+    access_token:         process.env.TWITTER_ACCESS_TOKEN || cred.twitter.access_token,
+    access_token_secret:  process.env.TWITTER_ACCESS_TOKEN_SECRET || cred.twitter.access_token_secret
 });
 
 var users = ['12265892', '15354314', '521522434'];
@@ -24,3 +29,8 @@ stream.on('tweet', function(tweet){
           });
 });
 
+
+function isHeroku()
+{
+  return process.env.NODE && ~process.env.NODE.indexOf("heroku") ? true : false;
+}
