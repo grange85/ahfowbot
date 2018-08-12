5bed05e22bba5ae714717ee981b2445d5cef6ba9
Andy Aldridge <andy@grange85.co.uk>
2018-08-12 01:38:55 +0100

    added express

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
 const fs = require('fs');
 const twit = require('twit');
-const express = require('express');
-const PORT = process.env.PORT || 5000;
-const app = express();
 
 
-//var cred = JSON.parse(fs.readFileSync('credentials.json'));
+var cred = JSON.parse(fs.readFileSync('credentials.json'));
 //console.log(cred.consumer_key);
 
 var t = new twit({
-    consumer_key:         process.env.TWITTER_CONSUMER_KEY,
-    consumer_secret:      process.env.TWITTER_CONSUMER_SECRET,
-    access_token:         process.env.TWITTER_ACCESS_TOKEN,
-    access_token_secret:  process.env.TWITTER_ACCESS_TOKEN_SECRET
+    consumer_key:         process.env.TWITTER_CONSUMER_KEY || cred.consumer_key,
+    consumer_secret:      process.env.TWITTER_CONSUMER_SECRET || cred.consumer_secret,
+    access_token:         process.env.TWITTER_ACCESS_TOKEN || cred.access_token,
+    access_token_secret:  process.env.TWITTER_ACCESS_TOKEN_SECRET || cred.access_token_secret
 });
 
 var users = ['12265892', '15354314', '521522434'];
